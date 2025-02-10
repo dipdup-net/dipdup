5f4e57a253b833b860fb4a807cd22a2362cb0dd0
Igor Sereda <sereda.igor.s@gmail.com>
2025-02-10 22:50:13 +0300

    Return etherlink test using mainnet

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -6,7 +6,9 @@ from decimal import Decimal
 from functools import partial
 
 import pytest
+from tortoise.functions import Sum
 
+import demo_tezos_etherlink.models
 from dipdup.database import tortoise_wrapper
 from dipdup.models.tezos import TezosOperationType
 from dipdup.test import run_in_tmp
@@ -172,6 +174,17 @@ async def assert_run_dao() -> None:
     assert votes == 1
 
 
+async def assert_run_etherlink() -> None:
+    query_set = demo_tezos_etherlink.models.Deposit.all()
+    deposits: int = await query_set.count()
+    tokens: list[str] = await query_set.distinct().values_list('token', flat=True)  # type: ignore
+    volume: int = await query_set.annotate(volume=Sum('amount')).first().values_list('volume', flat=True)  # type: ignore
+
+    assert deposits == 3
+    assert tokens == ['KT1MZg99PxMDEENwB4Fi64xkqAVh5d1rv8Z9']
+    assert volume == 15005
+
+
 test_args = ('config', 'package', 'cmd', 'assert_fn')
 test_params = (
     # NOTE: Tezos
@@ -183,9 +196,8 @@ test_params = (
     ('demo_tezos_dex.yml', 'demo_tezos_dex', 'init', None),
     ('demo_tezos_domains.yml', 'demo_tezos_domains', 'run', assert_run_domains),
     ('demo_tezos_domains.yml', 'demo_tezos_domains', 'init', None),
-    # FIXME: parisnet is dead
-    # ('demo_tezos_etherlink.yml', 'demo_tezos_etherlink', 'run', None),
-    # ('demo_tezos_etherlink.yml', 'demo_tezos_etherlink', 'init', None),
+    ('demo_tezos_etherlink.yml', 'demo_tezos_etherlink', 'run', None),
+    ('demo_tezos_etherlink.yml', 'demo_tezos_etherlink', 'init', None),
     ('demo_tezos_events.yml', 'demo_tezos_events', 'run', assert_run_events),
     ('demo_tezos_events.yml', 'demo_tezos_events', 'init', None),
     ('demo_tezos_factories.yml', 'demo_tezos_factories', 'run', assert_run_factories),
